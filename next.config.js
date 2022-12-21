c0232f049b18fa378c46bdd48528b37d8ab97fd8
Mayank <mayank@macbookair.com>
2022-12-21 18:11:01 +0530

    updated next js config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /* next.config.js */
 module.exports = {
-  // target: 'serverless', // <- add here
-  webpack: (config) => {
+  // target: "serverless", // <- add here
+
+  webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
       config.resolve.fallback.fs = false;
